98be175fc62f7a8e2ba6048ccc2dfab5f33558b4
Sollera <pjh27062706@naver.com>
2024-09-20 22:35:36 +0900

    2024.09.20

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
-const colorOptions = document.getElementsByClassName("color-option");
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+const colorOptions = Array.from( document.getElementsByClassName("color-option"));
 const color = document.getElementById("color");
 const lineWith = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWith.value;
 
 /*ctx.moveTo(50,50);
@@ -67,6 +74,7 @@ canvas.addEventListener("mousemove", onClick);*/
 
 ctx.lineWidth = 5;
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event){
     if(isPainting){
@@ -94,16 +102,58 @@ function onLineWidthChange(event){
 
 function onColorChange(event){
     ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event){
+    const colorValue = event.target.dataset.color;
+    //console.dir(event.target.dataset.color);
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
+}
+
+function onModeClick(event){
+    if(isFilling){
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    }
+    else{
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+
+function onCanvasClick(){
+    if(isFilling){
+        ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    }
+}
+
+function onDestroyClick(event){
+    //ctx.strokeStyle = "white";
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+}
+
+function onEraserClick(event){
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 //document.add("mouseleave", onMouseUp);
 
 lineWith.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
-console.log(colorOptions);
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
 
+modeBtn.addEventListener("click", onModeClick)
+destroyBtn.addEventListener("click", onDestroyClick)
+eraserBtn.addEventListener("click", onEraserClick)
